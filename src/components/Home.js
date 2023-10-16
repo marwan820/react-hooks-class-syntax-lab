af71713a9433380c6a2283137522306104b9706a
Marwan Rahim <marwan820@Marwan>
2023-10-16 11:01:40 -0500

    passed About tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React from "react"
 
-class  Home extends React.Component {
-  render(){
-  return (
-    <div id="home">
-      <h1 style={{ color: this.props.color }}>
-        {this.props.username} is a Web Developer from {this.props.city}
-      </h1>
-    </div>
-  );
-}}
+class Home extends React.Component {
+  render() {
+    return (
+      <div id="home">
+        <h1 style={{ color: this.props.color }}>
+          {this.props.username} is a Web Developer from {this.props.city}
+        </h1>
+      </div>
+    )
+  }
+}
 
-export default Home;
+export default Home
